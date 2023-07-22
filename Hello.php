a2e4d3b65cd60eb59efd36e29d4ad4e968e30b4b
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2023-07-22 15:54:42 +0100

    Update Hello.php

--- Hello.php
+++ Hello.php
@@ -9,11 +9,6 @@
 if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE) {
     echo 'You are using Google Chrome.<br />';
 }
-<br>
-elseif (strpos ($_SERVER['HTTP_USER_AGENT'], 'Edge' ) !== FALSE){
-    echo 'You are using Microsoft Edge.<br />';
-}
-
 ?>
 <a href="/Page2.php">page2</a>
 </body>
